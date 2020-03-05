478e3200db084ea2a9804f5958e76f6fed9e23fc
Innei <tukon479@gmail.com>
2020-03-05 08:51:19 +0800

    Initial Commit

---
+++ swiftUI-demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUI-demo
+//
+//  Created by Innei on 5/3/2020.
+//  Copyright © 2020 Innei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
